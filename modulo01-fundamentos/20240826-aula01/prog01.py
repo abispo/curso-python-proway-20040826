69ac06bda9db84cc5da68070886b114787a7512e
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2024-08-26 20:15:10 -0300

    Arquivos aula 01 python fundamentos

---
+++ modulo01-fundamentos/20240826-aula01/prog01.py
@@ -0,0 +1,10 @@
+
+nome = input("Informe o seu nome: ")
+print("Olá {sujeito}. {mensagem}".format(
+    sujeito=nome, mensagem="Bem-vindo")
+)
+
+# Isso é um comentário
+# nome é uma variável que recebe o valor retornado pela função built-in input(). Python é uma linguagem de tipagem dinâmica
+# A função built-in print imprime uma mensagem na tela. Aceita qualquer tipo de dado.
+# Podemos utilizar o método de strings 'format' para concatenar strings
---
+++ modulo01-fundamentos/20240826-aula01/prog02.py
@@ -0,0 +1,60 @@
+
+# Apesar de não ser obrigatório, é uma boa prática o arquivo principal da aplicação conter a condição abaixo.
+if __name__ == "__main__":
+
+    # Tipos de dados (string, int, list, dict, ...)
+    nome = "Python"     # str
+    numero1 = 10        # int
+    numero2 = 5.6       # float
+    numero3 = 3j        # complex
+    aberto = True       # boolean (True, False)
+    valor = None        # None é o null do Python
+
+    lista1 = ["Banana", "Abacaxi", "Uva"]           # Lista
+    dict1 = {"nome": "Alessandro", "curso": "Java"} # Dicionário
+    tupla1 = ("Maria", "João", "Carla",)            # Tupla
+    set1 = {1, 2, 2, 3}                             # Set (conjunto)
+
+    # Operadores da linguagem
+
+    # Operadores aritméticos
+    # +, -, *, /    -> Adição, Subtração, Multiplicação, Divisão
+    # %, **         -> Resto da divisão, exponenciação
+
+    # Operadores de atribuição
+
+    # Operadores de comparação
+    # ==    -> Igual a
+    # !=    -> Diferente de
+    # >     -> Maior que
+    # <     -> Menor que
+    # >=    -> Maior ou igual a
+    # <=    -> Menor ou igual a
+
+    # Operadores lógicos
+    # and   -> E                    &&
+    # or    -> OU                   ||
+    # not   -> Não (negação)        !
+
+    # Operadores de identidade
+    # is
+    # is not
+
+    # Operadores de pertencimento
+    # in
+    # not in
+
+    print(((4 * 4) + 4 - 8 ** 1) >= 6 ** 1)
+    print(5 * 4 + 3 / 2)
+
+    # Python é uma linguagem case-sensitive. Ou seja, a variável nome não é a mesma que a variável Nome
+
+
+    # Laços de condição (if, elif, else, case, ...)
+    # Laços de repetição (for, while)
+    # funções (def teste())
+    # classes
+    # bibliteca padrão
+
+
+    pass
